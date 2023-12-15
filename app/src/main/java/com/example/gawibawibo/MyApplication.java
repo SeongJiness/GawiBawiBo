72f9c138d47ac1633f14913c388bf2fa30ffefc3
SeongJiness <oke3016@naver.com>
2023-12-15 23:18:31 +0900

    가위바위보 찐 완성

--- app/src/main/java/com/example/gawibawibo/MyApplication.java
+++ app/src/main/java/com/example/gawibawibo/MyApplication.java
@@ -19,10 +19,6 @@ public class MyApplication extends Application {
         mMediaPlayer.start();
     }
 
-    public boolean isMusicPlaying() {
-        return mMediaPlayer != null && mMediaPlayer.isPlaying();
-    }
-
     public MediaPlayer getMediaPlayer() {
         return mMediaPlayer;
     }
